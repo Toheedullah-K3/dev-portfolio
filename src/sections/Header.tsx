9bb39f20b5c61445a1fac7914b989fe52b32cae4
Toheedullah-K3 <toheedullah002@gmail.com>
2024-10-13 23:23:32 +0500

    initial commit

---
+++ src/sections/Header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  return (
+    <div>
+      <nav>
+        <a href="#"> Home </a>
+        <a href="#"> Project </a>
+        <a href="#"> About </a>
+        <a href="#"> Contact </a>
+      </nav>
+    </div>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      fontFamily: {
+        sans: "var(--font-inter)",
+        serif: "var(--font-calistoga)",
+      }
     },
   },
+
   plugins: [],
 };
 export default config;
