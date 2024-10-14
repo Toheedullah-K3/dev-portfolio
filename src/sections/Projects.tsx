7f7b9e777ff1288a16b9f14c4b0a158acac00e66
Toheedullah-K3 <toheedullah002@gmail.com>
2024-10-15 03:25:06 +0500

    Editing Project Section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,6 +1,7 @@
 import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import Image from "next/image";
 
 const portfolioProjects = [
   {
@@ -42,5 +43,41 @@ const portfolioProjects = [
 ];
 
 export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
+  return (
+    <div>
+      <div className="container">
+        <div className="flex justify-center">
+          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text">Real-world Results</p>
+        </div>
+        <h2 className="font-serif text-3xl text-center mt-6">Featured Projects</h2>
+        <p className="text-center text-white/60 mt-4">
+          See how I transformed concepts into engaging digital experiences.
+        </p>
+      </div>
+
+      <div>
+        {portfolioProjects.map(project => (
+          <div key={project.title}>
+            <div>
+              <span> {project.company} </span>
+              <span> {project.year} </span>
+            </div>
+            <h3> {project.title} </h3>
+            <hr />
+            <ul>
+              {
+                project.results.map(result => (
+                  <li> {result.title} </li>
+                ))
+              }
+            </ul>
+            <a href={project.link}>
+              <button>View Live Site</button>
+            </a>
+            <Image src={project.image} alt={project.title} />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 };
