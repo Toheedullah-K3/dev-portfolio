05165554477f0449c3d1a0831d4c6e022355cf07
Toheedullah-K3 <toheedullah002@gmail.com>
2024-10-21 02:02:17 +0500

    Projects Section Animation Complete

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -61,10 +61,14 @@ export const ProjectsSection = () => {
 
 
         <div className="flex md:mt-20 flex-col mt-10 gap-20">
-          {portfolioProjects.map(project => (
+          {portfolioProjects.map((project, projectIndex) => (
             <Card 
               key={project.title} 
-              className="px-8 pt-8 md:pt-12 pb-0 md:px-10 lg:pt-16 lg:px-20">
+              className="px-8 pt-8 md:pt-12 pb-0 md:px-10 lg:pt-16 lg:px-20 sticky"
+              style={{
+                top: `calc(64px + ${projectIndex * 40}px)`
+              }}
+            >
 
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
